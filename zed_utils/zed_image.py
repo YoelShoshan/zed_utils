48c788b0af8f6d3a2b6c2bab5b546ee395982ba9
Yoel Shoshan <yoels@il.ibm.com>
2016-10-30 18:23:10 +0200

    added ability to do imshow of multiple images with shared axis

--- zed_utils/zed_image.py
+++ zed_utils/zed_image.py
@@ -17,6 +17,69 @@ def imshowpair(im1,im2, cmap1=None, cmap2=None):
 
     plt.show()
 
+#example usage:
+#imshowmult(img1,img2,img3,cmap1='gray',interpolation2='nearest',title1='banana')
+def imshowmult(*args, **kwargs):
+    images_num = len(args)
+    if 0==images_num:
+        raise Exception('No images provided!')
+
+    #for a in args:
+    #    print(a)
+    #for k, v in kwargs.iteritems():
+    #    print("%s = %s" % (k, v))
+
+    figures_side = np.ceil(np.sqrt(images_num))
+    fig = plt.figure()
+
+    base_axis = None
+
+    for i,img in enumerate(args):
+        sharex = None
+        sharey = None
+        if base_axis:
+            sharex = base_axis
+            sharey = base_axis
+
+        ax = fig.add_subplot(figures_side,figures_side, i+1, sharex=sharex, sharey=sharey)
+
+        if not base_axis:
+            base_axis = ax
+
+        cmap_str = 'cmap%d'%i
+        camp_val = None
+        if cmap_str in kwargs:
+            camp_val = kwargs[cmap_str]
+
+        interpolation_str = 'interpolation%d' % i
+        interpolation_val = None
+        if interpolation_str in kwargs:
+            interpolation_val = kwargs[interpolation_str]
+
+        ax.imshow(img, cmap=camp_val, interpolation=interpolation_val)
+
+    plt.show()
+
+def test_imshowmult():
+    import scipy.misc
+    import os
+    def compare(num):
+        input_filename = '%06d_INPUT_PATCH.png' % num
+        detection_filename = '%06d_PREDICTION_PATCH.png' % num
+        gt_mask_filename = '%06d_GT_MASK.png' % num
+        images_loc = '/gpfs/haifa/projects/m/msieve_dev2/usr/yoel/dev/dbs/MG/all_tumors_diseases/vis/prediction_fullimage'
+        input_img = scipy.misc.imread(os.path.join(images_loc,input_filename))
+        detection_img = scipy.misc.imread(os.path.join(images_loc,detection_filename))
+        detection_img = scipy.misc.imresize(detection_img, (6000, 6000))
+        gt_mask_img = scipy.misc.imread(os.path.join(images_loc,gt_mask_filename))
+        imshowmult(input_img, detection_img, gt_mask_img, detection_img > 1, cmap0='gray',
+                        interpolation1='nearest')
+    compare(35)
+
+
+#test_imshowmult()
+
+
 def savepng(filename,arr):
     if len(arr.shape) != 2:
         raise(ValueError('savepng only supports 2d numpy arrays'))
